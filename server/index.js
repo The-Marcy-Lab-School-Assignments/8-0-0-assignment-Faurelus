9de116c3473b3711c404c0a3805e014d2c2dfd60
benspector-mls <ben@marcylabschool.org>
2024-04-26 13:20:44 -0400

    code graded: 12/13 tests passing

--- server/index.js
+++ server/index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express()
 
 const serveInfo = (req, res, send) => {
-    res.send('<h1>Hello<h1>')
+  res.send('<h1>Hello<h1>')
 }
 const serveIndex = (req, res, next) => {
-    res.sendFile(__dirname + '/index.html');
-  }
-const serveWorld = (req, res, send) =>  {
-    res.send('World')
+  res.sendFile(__dirname + '/index.html');
+}
+const serveWorld = (req, res, send) => {
+  res.send('World')
 }
 const serveHello = (req, res, next) => {
-    const name = req.query.name || "stranger";
-    res.send(`hello ${name}`);
-  }
+  const name = req.query.name || "stranger";
+  res.send(`hello ${name}`);
+}
 
-  app.get('/', serveIndex)
-  app.get('/about', serveInfo)
-  app.get('/api/world', serveWorld)
-  app.get('/api/hello', serveHello)
+app.get('/', serveIndex)
+app.get('/about', serveInfo)
+app.get('/api/world', serveWorld)
+app.get('/api/hello', serveHello)
 const port = 8080;
-app.listen(port, () => console.log(`listening at http://localhost:${port}`)); 
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
 
 
 
